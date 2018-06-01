778f31c39e19411f7f7542afc7ebe84e43561a15
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2018-06-01 15:00:05 -0500

    Test of fixing deploy.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,6 +11,9 @@ source ci/functions.sh
 # Check that the Docker Hub organization to use is in the DOCKERHUB_ORG variable
 check_org
 
+branch=$(current_branch_name)
+echo $branch
+
 # Fetch the newest changes for DEPLOY_BRANCH
 compare_range=$(get_compare_range)
 
@@ -23,4 +26,4 @@ paths=$(changed_paths_in_range "$compare_range")
 # <tool>/<version>/Dockerfile. If none found, prints a message indicating so.
 # Push images that have changed to Docker hub.
 build_images "$DOCKERHUB_ORG" "$paths"
-push_images "$DOCKERHUB_ORG" "$paths"
+#push_images "$DOCKERHUB_ORG" "$paths"

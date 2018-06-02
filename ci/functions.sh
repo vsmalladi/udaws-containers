0d1ee23ff8a6d830cfe780d466084aa28a82e029
Venkat Malladi <vsmalladi@gmail.com>
2018-06-02 10:18:05 -0500

    Fix login.

--- ci/functions.sh
+++ ci/functions.sh
@@ -67,7 +67,7 @@ function get_compare_range() {
 function docker_login() {
   owner=$1
   passwd=$2
-  docker login -u $owner -p $passwd
+  echo $passwd | docker login -u $owner --password-stdin
 }
 
 # Given a docker repo owner, image name, and version, produce a local docker build command

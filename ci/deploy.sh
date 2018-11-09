0b7593ff498c64d6912bbd2d82c1cc72fe90e3bd
Venkat Malladi <vsmalladi@gmail.com>
2018-11-08 22:14:44 -0600

    Add in conditional filter for config. Remove rebuilding in deploy and test.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -18,9 +18,7 @@ compare_range=$(get_compare_range)
 # Get a list of changed paths in the repo to look for <tool>/<version>/Dockerfile
 paths=$(changed_paths_in_range "$compare_range")
 
-# Loop through the changed files and build Docker images for any that match
-# <tool>/<version>/Dockerfile. If none found, prints a message indicating so.
+
 # Push images that have changed to Docker hub.
-build_images "$DOCKERHUB_ORG" "$paths"
 echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 push_images "$DOCKERHUB_ORG" "$paths"
